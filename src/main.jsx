1f4716ab22787c0425baba22ed7e63546b520297
mishba-ai <mishbabagban@gmail.com>
2024-02-23 08:56:49 +0530

    update the path of the challenge page in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Home from "./Pages/HomePage.jsx";
 import Profile from "./Pages/ProfilePage.jsx";
 import Project from "./Pages/project.jsx";
 import Feed from "./Pages/Feed.jsx";
-import  Challenge from "./Pages/Challenge.jsx";
+import  Challenge from "src/Pages/Challenge.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
