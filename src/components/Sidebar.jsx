c66bf61f5cd6284ce514f110f0c8bd259880febd
mishba-ai <mishbabagban@gmail.com>
2024-02-27 10:43:03 +0530

    create the features in profile page and add the animation in the homepage

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -35,16 +35,16 @@ function Sidebar({ style }) {
           <ul className="pt-7 px- flex justify-center">
             <li>
               <Tooltip text="User">
-              <button
-                className={` ${
-                  isCurrentPath("/userplus")
-                    ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(255,255,255)] active translate-y-[-3px] "
-                    : "bg-transparent rounded-2xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
-                }`}
-              > 
-                <img src={userplus} alt="image" className="h-8 w-8  p-1" />
-                
-              </button></Tooltip>
+                <button
+                  className={` ${
+                    isCurrentPath("/userplus")
+                      ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(255,255,255)] active translate-y-[-3px] "
+                      : "bg-transparent rounded-2xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
+                  }`}
+                >
+                  <img src={userplus} alt="image" className="h-8 w-8  p-1" />
+                </button>
+              </Tooltip>
             </li>
           </ul>
           <hr className="border-gray-300 m-3" />
@@ -54,80 +54,81 @@ function Sidebar({ style }) {
               {" "}
               <li className=" pt-3">
                 <Tooltip text="dashboard">
-                <button
-                  className={` ${
-                    isCurrentPath("/Home")
-                      ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1  translate-y-[-3px]"
-                      : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
-                  }`}
-                >
-                  <img src={dashboard} alt="dash" />
-                </button></Tooltip>
+                  <button
+                    className={` ${
+                      isCurrentPath("/Home")
+                        ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1  translate-y-[-3px]"
+                        : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
+                    }`}
+                  >
+                    <img src={dashboard} alt="dash" />
+                  </button>
+                </Tooltip>
                 {/* <p>Dashboard</p> */}
               </li>
             </Link>
             <Link to="/Project">
               <li className="">
-              <Tooltip text="Project">
-
-                <button
-                  className={` ${
-                    isCurrentPath("/Project")
-                      ? "transition-all translate-x-[-3px] rounded-xl px-1 py-1 shadow-[2px_2px_0px_rgb(206,255,26)]  translate-y-[-3px]"
-                      : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
-                  }`}
-                >
-                  <img src={project} alt="pjt" />
-                </button></Tooltip>
+                <Tooltip text="Project">
+                  <button
+                    className={` ${
+                      isCurrentPath("/Project")
+                        ? "transition-all translate-x-[-3px] rounded-xl px-1 py-1 shadow-[2px_2px_0px_rgb(206,255,26)]  translate-y-[-3px]"
+                        : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
+                    }`}
+                  >
+                    <img src={project} alt="pjt" />
+                  </button>
+                </Tooltip>
                 {/* <p>Projects</p> */}
               </li>
             </Link>
             <Link to="/Challenges">
               <li className="">
-              <Tooltip text="Challenge">
-
-                <button
-                  className={` ${
-                    isCurrentPath("/Challenge")
-                      ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
-                      : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px] "
-                  }`}
-                >
-                  <img src={challenge} alt="chg" />
-                </button></Tooltip>
+                <Tooltip text="Challenge">
+                  <button
+                    className={` ${
+                      isCurrentPath("/Challenges")
+                        ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
+                        : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px] "
+                    }`}
+                  >
+                    <img src={challenge} alt="chg" />
+                  </button>
+                </Tooltip>
                 {/* <p>Challenges</p> */}
               </li>
             </Link>
             <Link to="/Feed">
               <li className="">
-              <Tooltip text="Feed">
-
-                <button
-                  className={` ${
-                    isCurrentPath("/Feed")
-                      ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
-                      : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
-                  }`}
-                >
-                  <img src={explore} alt="exp" />
-                </button></Tooltip>
+                <Tooltip text="Feed">
+                  <button
+                    className={` ${
+                      isCurrentPath("/Feed")
+                        ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
+                        : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
+                    }`}
+                  >
+                    <img src={explore} alt="exp" />
+                  </button>
+                </Tooltip>
                 {/* <p>Explore</p> */}
               </li>
             </Link>
             <Link to="/Profile">
               <li className="">
-              <Tooltip text="Profile">
-
-                <button
-                  className={` ${
-                    isCurrentPath("/Profile")
-                      ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
-                      : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
-                  }`}
-                >
-                  {" "}
-                  <img src={profile} alt="pfp" />
-                </button></Tooltip>
+                <Tooltip text="Profile">
+                  <button
+                    className={` ${
+                      isCurrentPath("/Profile")
+                        ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
+                        : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px]"
+                    }`}
+                  >
+                    {" "}
+                    <img src={profile} alt="pfp" />
+                  </button>
+                </Tooltip>
                 {/* <p>Profile</p> */}
               </li>
             </Link>
@@ -136,17 +137,18 @@ function Sidebar({ style }) {
             <hr className="border-gray-300 mx-3" />{" "}
             <Link to="/Logout">
               <div className="mt-4 flex  justify-center ">
-              <Tooltip text="Logout">
-                <button
-                  className={` ${
-                    isCurrentPath("/Logout")
-                      ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
-                      : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px] mt-2"
-                  }`}
-                >
-                  {" "}
-                  <img src={logout} alt="lout" className="rotate-180" />
-                </button></Tooltip>
+                <Tooltip text="Logout">
+                  <button
+                    className={` ${
+                      isCurrentPath("/Logout")
+                        ? "transition-all translate-x-[-3px] shadow-[2px_2px_0px_rgb(206,255,26)] rounded-xl px-1 py-1 translate-y-[-3px]"
+                        : "bg-transparent rounded-xl px-1 py-1  transition-all hover:translate-x-[-3px] hover:shadow-[2px_2px_0px_rgb(255,255,255)] hover:translate-y-[-3px] mt-2"
+                    }`}
+                  >
+                    {" "}
+                    <img src={logout} alt="lout" className="rotate-180" />
+                  </button>
+                </Tooltip>
                 {/* <p>Logout</p> */}
               </div>
             </Link>
