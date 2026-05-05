043f0593e525c794d7374d9c808d0f846fe43bf3
mishba-ai <mishbabagban@gmail.com>
2026-05-05 18:16:16 +0530

    Update logo references in Footerla and Headerlanding components for consistency

--- frontend/src/components/landing/Headerlanding.tsx
+++ frontend/src/components/landing/Headerlanding.tsx
@@ -1,15 +1,15 @@
-import connecthlogo from "/connecthlogo.png";
+import connectlogo from "/connectlogo.png";
 import GoogleLoginbtn from "./GoogleLoginbtn.tsx";
 
 export const Headerlanding = () => {
   return (
-    <div className="flex justify-between w-full h-16 px-10 py-2 m-0">
-      <div className="-mt-2">
-        <img src={connecthlogo} alt="logo" className=" h-20" />
+    <div className="flex justify-between w-full h-16 px-10 py-2 pt-2">
+      <div className="-">
+        <img src={connectlogo} alt="logo" className=" h-8" />
       </div>
       <div className="flex gap-x-6 mt-2">
         <GoogleLoginbtn/>
-        <h3 className="mt-2 font-semibold text-lg text-white font-PressStart">About</h3>
+        {/* <h3 className="mt-2 font-semibold text-lg text-white font-PressStart">About</h3> */}
       </div>
     </div>
   );
